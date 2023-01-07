26148ea3824f38521727e38a6f9bc2b38ca66a32
Paulo Rogerio Gomes E Silva <silva.paulo@ifood.com.br>
2023-01-06 22:19:58 -0300

    Server

---
+++ 14-server-http-v2/server/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	// "encoding/json"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// Monitor Struct
+type Monitor struct {
+	Id   string `json:id`
+	Name string `json:name`
+}
+
+// Init Monitors ( Dados Mock )
+var monitors []Monitor
+
+// Get All Monitors
+func getMonitors(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(monitors)
+}
+
+// Get Single Monitor
+func getMonitor(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range monitors {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Monitor{})
+}
+
+// Create Single Monitor
+func createMonitor(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var monitor Monitor
+	_ = json.NewDecoder(r.Body).Decode(&monitor)
+	monitor.Id = strconv.Itoa(rand.Intn(10000))
+	monitors = append(monitors, monitor)
+	json.NewEncoder(w).Encode(monitor)
+}
+
+func main() {
+
+	// Init Router
+	r := mux.NewRouter()
+
+	// parei aqui
+	monitors = append(monitors, Monitor{Id: "1", Name: "monitor-teste-123"})
+
+	// Router Handlers / Endpoints
+	r.HandleFunc("/api/monitors", getMonitors).Methods("GET")
+	r.HandleFunc("/api/monitors/{id}", getMonitor).Methods("GET")
+	r.HandleFunc("/api/monitors", createMonitor).Methods("POST")
+
+	// Listen Server
+	fmt.Println("Listen in :3000")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
